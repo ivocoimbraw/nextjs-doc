bb1e7f58ee25f66e48af88bcb04e027c22812766
ivocoimbraw <wolfcoimbraivo@gmail.com>
2025-03-27 10:41:31 -0400

    next-doc

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitanaNormal = Lusitana({  subsets: ['latin'], weight:'400'  });
+export const lusitanaBold = Lusitana({  subsets: ['latin'], weight:'700'  });
